04b5a57c05c75c5910828a35ded67656ffd81075
SherinJM <115409980+SherinJM@users.noreply.github.com>
2023-02-18 17:46:05 +0530

    Adding course

---
+++ src/test/java/testcases/CreateCourse.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+import dataProvider.CustomDataProvider;
+import pages.LoginPage;
+
+public class CreateCourse extends BaseClass {
+
+	@Test
+	public void loginToApplication(String uName, String password) throws Exception {
+
+	}
+
+}
